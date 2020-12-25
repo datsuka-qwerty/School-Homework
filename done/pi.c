8559d1c8309b741ae84786afe49628e9c5256949
Datsuka <49899602+datsuka-qwerty@users.noreply.github.com>
2020-12-25 22:59:21 +0900

    [update]Update pi.c
    Upgrade result

--- done/pi.c
+++ done/pi.c
@@ -15,8 +15,8 @@ int main(void)
 	pi_rect = intgr_rect(n);
 	pi_trape = intgr_trape(n);
 
-	printf("結果は\n短形：%lf 誤差：%lf\n台形：%lf 誤差：%lf\n", pi_rect, (pi_rect-M_PI), pi_trape, (pi_trape-M_PI));
-	printf("数学関数で定義されているπ：%lf\n", M_PI);
+	printf("結果は\n短形：%.15f\n台形：%.15f\n", pi_rect, pi_trape);
+	printf("数学関数で定義されているπ：%.15f\n", M_PI);
 	return 0;
 }
 
@@ -24,7 +24,7 @@ int main(void)
 double intgr_rect(double n) // find the area of a quarter of a circle by "rectangle"
 {
 	double area = 0;
-	for(int i = 1; i < n+1; i++)
+	for(int i = 1; i < n+1.0; i++)
 	{
 		double x = (i/n);
 		area += ((1.0/n)*(sqrt(1.0-(x*x))));
@@ -36,7 +36,7 @@ double intgr_rect(double n) // find the area of a quarter of a circle by "rectan
 double intgr_trape(double n) // find the area of a quarter of a circle by "trapezoid"
 {
 	double area = 0;
-	for (int i = 1; i < n+1; i++)
+	for (int i = 1; i < n+1.0; i++)
 	{
 		double y1, y2 = 0; // reset left point y1, right y2
 		double x = (i/n); // reset x coordinate
